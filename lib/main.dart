d683309807e0feca224c6ffb15a2185e0564755f
Alaa-Elhfnawy <alabdalrhym791@gmail.com>
2023-10-17 23:58:39 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthDayCard());
+}
+
+class BirthDayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xffD2BCD5),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/birthday.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
